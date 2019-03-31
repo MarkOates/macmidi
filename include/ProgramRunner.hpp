d46888e723fcd7c0d8e08ebf9ae79e98c9697cb3
Mark Oates <markoates0@gmail.com>
2019-03-31 18:50:57 -0400

    Add initial generated files

---
+++ include/ProgramRunner.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+
+#include <string>
+
+
+class ProgramRunner
+{
+private:
+
+public:
+   ProgramRunner();
+   ~ProgramRunner();
+
+
+std::string run();
+};
+
+
+
---
+++ programs/main.cpp
@@ -0,0 +1,8 @@
+
+#include <ProgramRunner.hpp>
+
+int main(int argc, char **argv)
+{
+   ProgramRunner().run();
+   return 0;
+}
