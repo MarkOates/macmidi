bd4b285be9eea6b111cd67c6beb0c593e1dd01ae
Mark Oates <markoates0@gmail.com>
2019-03-31 23:02:57 -0400

    Remove extranious MIDI code from MacMidi

--- tests/MacMidiTest.cpp
+++ tests/MacMidiTest.cpp
@@ -9,9 +9,3 @@ TEST(MacMidiTest, initialize__works_without_raising_an_exception)
    program_runner.initialize();
 }
 
-TEST(MacMidiTest, get_num_devices__returns_the_number_of_connected_midi_devices)
-{
-   MacMidi mac_midi;
-   ASSERT_EQ(3, mac_midi.get_num_devices());
-}
-
